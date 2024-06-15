3c28160070ab934945a2d44fca801a9b33b3eda4
Bruce <bruce.fan0308@gmail.com>
2024-06-15 17:23:48 -0400

    [UPDATE] daily leetcode

---
+++ LC521.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        return -1 if a == b else max(len(a), len(b))
+
+if __name__ == '__main__':
+    Solution.findLUSlength("aba", "cdc")
