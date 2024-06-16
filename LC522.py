7fb45a399e1843063a112ce4a94d99960c5dd873
Bruce <bruce.fan0308@gmail.com>
2024-06-16 14:53:03 -0400

    [UPDATE] daily LeetCode

---
+++ LC522.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findLUSlength(self, strs):
+        def subseq(w1, w2):
+            if len(w1) > len(w2):
+                return False
+            i = 0
+            for c in w2:
+                if i < len(w1) and w1[i] == c:
+                    i += 1
+            return i == len(w1)
+
+        strs.sort(key=len, reverse=True)
+        for i, word1 in enumerate(strs):
+            if all(not subseq(word1, word2)
+                   for j, word2 in enumerate(strs) if i != j):
+                return len(word1)
+        return -1
+
+if __name__ == '__main__':
+    Solution.findLUSlength("aaa", "aaa", "aa")
